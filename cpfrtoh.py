3a85a8c0a1784d63202d4c87af9c0f7789d6604a
Nate Aquino <naquino14@outlook.com>
2023-04-04 10:08:54 -0400

    Finalized ssv to header script

--- cpfrtoh.py
+++ cpfrtoh.py
@@ -5,11 +5,17 @@ import os
 def main():
     with open('frames.ssv') as f:
         lines = f.readlines()
+    c = 0
+    lc = 0
     with open('frames.h', 'w') as f:
-        f.write('const char *FRAMES = \"')
+        f.write('#define FRAMES \"')
         for line in lines:
-            f.write(line.strip() + '\\n')
-        f.write('\";')
+            # write the line, with a newline if it's not the last line
+            f.write(line.strip() if lc == len(lines) - 1 else line.strip() + '\\n')
+            c += len(line)
+            lc += 1
+        f.write('\"\n')
+        f.write(f'#define FRAMES_LEN {c + 2}')
     os.system('cd ./build && make')
     return 0
 

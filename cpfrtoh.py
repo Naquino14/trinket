0f1bba015287bb94ef1a818c5b82cfacf1751e8e
Nate Aquino <naquino14@outlook.com>
2023-04-04 20:25:31 -0400

    added parameterized file picking to cpfrtoh

--- cpfrtoh.py
+++ cpfrtoh.py
@@ -1,9 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# CoPy FRames TO Header
 
 import os
+import sys
 # turns frames.ssv into frames.h
-def main():
-    with open('frames.ssv') as f:
+def main(argc, argv):
+    if argc != 2:
+        print('Usage: cpfrtoh.py [file]')
+        return 1
+    with open(argv[1]) as f:
         lines = f.readlines()
     c = 0
     lc = 0
@@ -15,9 +20,9 @@ def main():
             c += len(line)
             lc += 1
         f.write('\"\n')
-        f.write(f'#define FRAMES_LEN {c + 2}')
+        f.write(f"#define FRAMES_LEN {c + 2}")
     os.system('cd ./build && make')
     return 0
 
 if __name__ == '__main__':
-    main()
+    main(len(sys.argv), sys.argv)
